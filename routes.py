65fa698afe101ef2889006a7bbccebc7209b493b
kievnnn-cell <kievnnn@gmail.com>
2026-05-31 00:14:44 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, request
+import telebot
+import os
+
 from bot import bot
 
-webhook_blueprint = Blueprint("webhook", __name__)
+webhook = Blueprint("webhook", __name__)
 
-@webhook_blueprint.route("/webhook", methods=["POST"])
-def webhook():
+@webhook.route("/webhook", methods=["POST"])
+def hook():
     json_str = request.get_data().decode("utf-8")
     update = telebot.types.Update.de_json(json_str)
     bot.process_new_updates([update])
