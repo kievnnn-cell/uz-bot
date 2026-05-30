eafc8980186bb9bdea55922c2f1af21ed0a5f852
kievnnn-cell <kievnnn@gmail.com>
2026-05-31 00:14:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,13 +4,6 @@ import os
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
-from services.uz_api import start_monitor
-
-@bot.message_handler(func=lambda m: True)
+@bot.message_handler(func=lambda message: True)
 def handle(message):
-    text = message.text.lower()
-
-    # пример команды
-    if "поезд" in text:
-        start_monitor(bot, message.chat.id, text)
-        bot.send_message(message.chat.id, "📡 Мониторинг запущен")
+    bot.send_message(message.chat.id, f"📩 Получено: {message.text}")
